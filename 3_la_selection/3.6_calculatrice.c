f875cf729797852b289d1f39afb0a6979d9d2d12
christophe-ye-biname <54074291+christophe-ye-biname@users.noreply.github.com>
2025-10-06 16:53:39 +0200

    Update 3.6_calculatrice.c

--- 3_la_selection/3.6_calculatrice.c
+++ 3_la_selection/3.6_calculatrice.c
@@ -5,10 +5,30 @@ int main(void) {
     char operateur;
 
     // TODO: récupérer le calcul
-
+    printf("nombre 1");
+    scanf("%i", &nombre1);
+    printf("nombre 2");
+    scanf("%i", &nombre2);
+    printf("operateur");
+    scanf("%c", &operateur);
     // TODO: calculer le résultat
-
-    // TODO: afficher le résultat
+    switch (operateur) {
+  case '+':
+    // code block
+    resultat = nombre1 + nombre2;
+    break;
+  case '-':
+    // code block
+    resultat = nombre1 - nombre2;
+    break;
+  case '*':
+    // code block
+    resultat = nombre1 * nombre2;
+    break;
+  case '/':
+    // code block
+    resultat = nombre1 / nombre2;
 
     return 0;
+}
 }
