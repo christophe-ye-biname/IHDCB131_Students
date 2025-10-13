c89df6a4afd7e1b6d2b909b4ee0cc8fd764ff97d
christophe-ye-biname <54074291+christophe-ye-biname@users.noreply.github.com>
2025-10-13 22:08:32 +0200

    Update 4.6_somme_du_cube.c

--- 4_l_iteration/4.6_somme_du_cube.c
+++ 4_l_iteration/4.6_somme_du_cube.c
@@ -3,14 +3,22 @@
 
 int main(void) {
     int nombre, a, b;
-    bool solution_trouvee;
-
+    bool solution_trouvee = false;
+    a = 0;
     // TODO: afficher une phrase permettant d'encoder le nombre et récupérer sa valeur
-
+    printf("Entrez un nombre");
+    scanf("%i", &nombre);
     // TODO: chercher et trouver si nombre peut être décomposé en la somme de deux cubes de deux nombres positifs et si oui, récupérer ces deux nombres et changer la valeur de solution_trouvee
-
+    int count = 0;
+    while (count < nombre/5 || solution_trouvee == false){
+        if (count*count*count == nombre){
+            b = count;
+            solution_trouvee = true;
+        }
+        count++;
+    }
     if (solution_trouvee) {
-        printf("%d est la somme du cube de %d et de %d\n.", nombre, a, b);
+        printf("%d est la somme du cube de %d et de %d.\n", nombre, a, b);
     } else {
         printf("Aucune paire de nombres positifs n'a été trouvée.\n");
     }
